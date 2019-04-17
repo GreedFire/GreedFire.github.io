a8ea63d4daffd983e3f121ec388696a6305ed0ee
GreedFire <greedofiro@gmail.com>
2019-04-17 10:07:23 +0200

    xxx

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://intense-fortress-76829.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://intense-fortress-76829.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
